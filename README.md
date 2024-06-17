932fde31017931e0a4610ec177fab7c381fb6b3b
Marcos Felipe <marcos.felipe@usp.br>
2024-06-17 19:44:06 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# DumbNotes.nvim
+
+DumbNotes.nvim is a simple Neovim plugin for managing and searching your
+notes.
+
+## Features
+
+- Create new notes in any format you choose (defaults to markdown)
+- Delete existing notes
+- List all notes using fd
+- Search through notes using ripgrep
+
+## Installation
+
+### Using Lazy
+
+Add the following to your `init.lua`
+```lua
+use {
+  'omarcospo/dumbnotes.nvim',
+  requires = { {'nvim-telescope/telescope.nvim'} }
+}
+```
+
+## Usage
+
+### Commands
+
+- `:DumbNotes` - List all notes in the notes directory.
+- `:DumbNotesGrep` - Search through notes using ripgrep.
+
+### Keybindings
+
+Within the Telescope prompt for notes:
+
+- `<CR>` - Open the selected note.
+- `<C-n>` - Create a new note.
+- `<C-d>` - Delete the selected note.
+- `<C-f>` - Search notes using ripgrep.
+
+## Default config
+
+Add the following setup code to your `init.lua` or `init.vim`:
+
+```lua
+require('dumbnotes').setup({
+  notes_format = "md", -- Format of the notes
+  notes_path = "~/Notes", -- Path to the notes directory
+  mappings = {
+    new_note_key = "<C-n>", -- Keybinding to create a new note
+    delete_note_key = "<C-d>", -- Keybinding to delete a note
+    search_note_key = "<C-f>", -- Keybinding to search notes
+  },
+})
+```
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
+
+## Contributing
+
+Contributions are welcome! Please feel free to submit a Pull Request.
---
+++ lua/dumbnotes.lua
@@ -0,0 +1,113 @@
+local dumbnotes = {}
+local opts = {}
+
+dumbnotes.config = {
+  notes_format = "md",
+  notes_path = "~/Notes",
+  mappings = {
+    new_note_key = "<C-n>",
+    delete_note_key = "<C-d>",
+  },
+}
+
+dumbnotes.setup = function(user_opts)
+  _G.dumbnotes = dumbnotes
+  opts = vim.tbl_deep_extend("force", dumbnotes.config, user_opts or {})
+  vim.api.nvim_create_user_command("DumbNotes", function()
+    dumbnotes.list_notes(opts)
+  end, {})
+  vim.api.nvim_create_user_command("DumbNotesGrep", function()
+    dumbnotes.search_notes(opts)
+  end, {})
+end
+
+function dumbnotes.list_notes(opts)
+  local actions = require("telescope.actions")
+  local action_state = require("telescope.actions.state")
+
+  local function open(input, ext, opts)
+    local path
+    if input == true or input ~= "" then
+      if ext then
+        path = vim.fn.expand(opts.notes_path .. "/" .. input .. "." .. opts.notes_format)
+        vim.cmd("edit " .. vim.fn.fnameescape(path))
+      else
+        path = vim.fn.expand(opts.notes_path .. "/" .. input)
+        if vim.fn.filereadable(path) == 1 then
+          vim.cmd("edit " .. vim.fn.fnameescape(path))
+        else
+          print("Invalid note: " .. path)
+        end
+      end
+    else
+      print("Invalid note.")
+    end
+  end
+
+  local function open_note(prompt_bufnr, opts)
+    local selection = action_state.get_selected_entry()
+    actions.close(prompt_bufnr)
+    open(selection[1], false, opts)
+  end
+
+  local function delete_note(prompt_bufnr, opts)
+    local selection = action_state.get_selected_entry()
+    local file_path = vim.fn.expand(opts.notes_path .. "/" .. selection[1])
+    actions.close(prompt_bufnr)
+    if vim.fn.filereadable(file_path) == 1 then
+      local confirm = vim.fn.confirm("Delete note: " .. file_path .. "?", "&Yes\n&No")
+      if confirm == 1 then
+        local ok, err = os.remove(file_path)
+        if not ok then
+          print("Error deleting note: " .. err)
+        else
+          print("Deleted note: " .. file_path)
+        end
+      else
+        print("Deletion cancelled.")
+      end
+    else
+      print("Note not found: " .. file_path)
+    end
+  end
+
+  local function new_note_prompt(prompt_bufnr, opts)
+    actions.close(prompt_bufnr)
+    local ok, input = pcall(function()
+      return vim.fn.input({ prompt = "Note title: " })
+    end)
+    if ok then
+      open(input, true, opts)
+    else
+      print("Error creating note: " .. input)
+    end
+  end
+
+  require("telescope.builtin").find_files({
+    prompt_title = "Notes",
+    cwd = opts.notes_path,
+    find_command = { "fd", "-d", "1", "--extension", opts.notes_format },
+    attach_mappings = function(prompt_bufnr, map)
+      map({ "i", "n" }, "<CR>", function()
+        open_note(prompt_bufnr, opts)
+      end)
+      map({ "i", "n" }, opts.mappings.delete_note_key, function()
+        delete_note(prompt_bufnr, opts)
+      end)
+      map({ "i", "n" }, opts.mappings.new_note_key, function()
+        new_note_prompt(prompt_bufnr, opts)
+      end)
+      return true
+    end,
+  })
+end
+
+function dumbnotes.search_notes(opts)
+  require("telescope.builtin").live_grep({
+    prompt_title = "Search Notes",
+    cwd = opts.notes_path,
+    glob_pattern = "*." .. opts.notes_format,
+  })
+end
+
+return dumbnotes
